7ef8ec112d4a183bfd7a50475b76fa3cd9f8a5b7
waime <waime@gmail.com>
2024-10-29 10:24:17 +0100

    new

--- SAS origin version.c
+++ SAS origin version.c
@@ -22,7 +22,7 @@ void creation(tache tab[], int n) {
 
     for (i = 0; i < n; i++) {
         printf("Entrer le titre de cette tache: ");
-        scanf(" %[^\n]", tab[i].name); // Utilisation de %[^\n] pour lire la ligne complète
+        scanf(" %[^\n]", tab[i].name); // Utilisation de %[^\n] pour lire l'espace est afficher la phrase complet
         printf("Entrer une description pour cette tache: ");
         scanf(" %[^\n]", tab[i].description);
         printf("Entrer le jour: ");
@@ -35,12 +35,46 @@ void creation(tache tab[], int n) {
         scanf(" %[^\n]", tab[i].priorite);
     }
 }
+// fonction d'affichade d'une tache
+void affichage(tache tab[],int n){
+    int i;
+
+    for (i = 0; i < n; i++) {
+      printf("Tritre:%s\n",tab[i].name);
+      printf("description:%s\n",tab[i].description);
+      printf("date:%d/%d/%d\n",tab[i].date.jour,tab[i].date.mois,tab[i].date.annee);
+      printf("priorite:%s\n",tab[i].priorite);
+
+}}
+
+// fonction de modification d'une tache
+void modification(tache tab[],int n){
+    int i;
+    printf("entrer l'indice de la tache que vouler modifier:");
+    scanf("%d",&i);
+    if (i<=0||i>n) //l'indice il faut pas depasse n et pas moins ou egale  0
+        printf("Indice non trouve, veuillez entrer un autre :");
+    else {
+         printf("Entrer un autre titre pour cette tache: ");
+        scanf(" %[^\n]", tab[i-1].name);
+        printf("Entrer une autre description pour cette tache: ");
+        scanf(" %[^\n]", tab[i-1].description);
+        printf("Entrer le jour: ");
+        scanf("%d", &tab[i-1].date.jour);
+        printf("Entrer le mois: ");
+        scanf("%d", &tab[i-1].date.mois);
+        printf("Entrer l'annee: ");
+        scanf("%d", &tab[i-1].date.annee);
+        printf("Entrer la priorite de cette tache: ");
+        scanf(" %[^\n]", tab[i-1].priorite);
+    }
+}
 
 int main(void) {
     tache tab[100];
     int choix, n;
 
-    printf("Combien de tâches voulez-vous créer ? ");
+    printf("Combien des taches voulez-vous creer ?: ");
     scanf("%d", &n);
 
     while (choix != 6) {
@@ -59,10 +93,12 @@ int main(void) {
             case 1:
                 creation(tab, n);
                 break;
-
-
-
-
+            case 2:
+               affichage(tab,n);
+               break;
+            case 3:
+                modification(tab,n);
+            break;
 
         case 6:
                 printf("Quitter le programme.\n");
